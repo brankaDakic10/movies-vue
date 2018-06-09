3b66db37510f2b111ff27b3ffd043e847810637f
brankaDakic10 <branka.dakic@yahoo.com>
2018-06-09 18:11:27 +0200

    7.Task

--- src/components/MovieRow.vue
+++ src/components/MovieRow.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="row">
+  <div class="row" :class="{ 'highlighted': selected }">
     <div class="col-sm" v-text="movie.id"></div>
     <div class="col-sm" v-text="movie.title"></div>
     <div class="col-sm" v-text="movie.director"></div>
     <div class="col-sm" v-text="movie.genre"></div>
     <div class="col-sm" v-text="movie.releaseDate"></div>
     <div class="col-sm" v-text="movie.duration"></div>
+  <div class="col-sm">
+  <b-button type="submit"
+   variant="primary"
+    @click="onSelected">
+      Select
+      </b-button>
+  </div>
   </div>
 </template>
 
@@ -17,10 +24,24 @@ props:{
       type: Object,
       required:true
       }
+},
+data(){
+  return{
+   selected:false
+  }
+},
+methods:{
+  onSelected(){
+     this.selected = true
+  this.$emit('on-selected-movie', this.movie)
+  }
 }
 }
 </script>
 
 <style>
+.highlighted{
+   background: rgb(179, 173, 173);
+}
 
 </style>
--- src/pages/AppMovies.vue
+++ src/pages/AppMovies.vue
@@ -1,13 +1,37 @@
 <template>
-    <div class="container">
-       <h3>List of Movies</h3>
-       <movie-search
+  <div class="container">
+
+    <movie-search
       @search-term-change="onSearchTermChanged"
-/>      
- <div class="container">                                 <!-- added props with data -->
-      <movie-row v-for="movie in movies" :key="movie.id" :movie="movie"/>
- </div> 
+      class="mt-4"
+    />
+
+    <div class="pt-3">
+      <b-badge
+        pill
+        variant="primary"
+        v-if="movies.length"
+      >
+        Selected: {{ selectedMoviesCounter }}
+      </b-badge>
+
+
+      <movie-row
+        v-for="movie in movies"
+        :key="movie.id"
+        :movie="movie"
+        @on-selected-movie="onSelectedMovie"
+      />
+
+      <b-alert
+        show
+        variant="warning"
+        v-if="!movies.length"
+      >
+        No Movies
+      </b-alert>
     </div>
+  </div>
 </template>
 
 <script>
@@ -24,26 +48,39 @@ components:{
 },
 data(){
     return{
-        movies:[]
+        movies:[],
+        selectedMoviesIds: [],
     }
+},
+computed:{
+selectedMoviesCounter(){
+     return this.selectedMoviesIds.length
+}
 },
 methods:{
      onSearchTermChanged(term) {
-      MoviesService.getAll(term)
+      MoviesService.get(term)
         .then(({ data }) => {
           this.movies = data
         })
+    },
+      onSelectedMovie(movie) {
+      if (this.selectedMoviesIds.indexOf(movie.id) > -1) {
+        return;
+      }
+      this.selectedMoviesIds.push(movie.id)
     }
 },
 
 
- beforeRouteEnter (to, from, next) {
+
+ beforeRouteEnter(to, from, next) {
     MoviesService.getAll()
-      .then(({data}) => {
-        next((vm) => {
-          vm.movies = data
-        })
+    .then(({ data }) => {
+      next((context) => {
+        context.movies = data;
       })
+    })
   }
 
 }
