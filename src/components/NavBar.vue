298555cd5dfb90c496fdd1551641f7a37eefe484
brankaDakic10 <branka.dakic@yahoo.com>
2018-06-09 09:34:42 +0200

    1.Task

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark movies-nav">
+            <router-link class="navbar-brand" :to="{name:'home'}">AppMovies</router-link>
+            <div class="navbar-nav w-100">
+                <router-link class="nav-item nav-link" :to="{name:'movies'}">Movies</router-link>
+                <div class="navbar-nav"></div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+.movies-nav{
+    background-color: crimson;
+}
+</style>
---
+++ src/pages/AppMovies.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        App Movies
+    </div>
+</template>
+
+<script>
+export default {
+name:"AppMovies"
+}
+</script>
+
+<style>
+
+</style>
