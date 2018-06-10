a6de7d6e67c97e40736207b7083976bbf863e0c2
brankaDakic10 <branka.dakic@yahoo.com>
2018-06-10 22:32:13 +0200

    Added guards.js and changes in index.js-router

--- src/router/index.js
+++ src/router/index.js
@@ -6,42 +6,64 @@ import AddMovie from '../pages/AddMovie'
 import AppLogin from '../pages/AppLogin'
 import AppRegister from '../pages/AppRegister'
 import AppSingleMovie from '../pages/AppSingleMovie'
+import {
+    requiresAuth,
+    guestOnly
+} from './guards'
 
 Vue.use(VueRouter)
 
 const routes = [{
     path: '/',
     redirect: '/movies',
-    name:'home'
+    name:'home',
+    meta: {
+        requiresAuth: true
+    }
 },
 {
     path: '/movies',
     component: AppMovies,
-    name: 'movies'
+    name: 'movies',
+    meta: {
+        requiresAuth: true
+    }
     
 },
 {
     path: '/movies/:id',
     component: AppSingleMovie,
-    name: 'single-movie'
+    name: 'single-movie',
+    meta: {
+        requiresAuth: true
+    }
     
 },
 {
     path: '/add',
     component: AddMovie,
-    name: 'add'
+    name: 'add',
+    meta: {
+        requiresAuth: true
+    }
     
 },
 {
     path: '/login',
     component: AppLogin,
-    name: 'login'
+    name: 'login',
+    meta: {
+        guestOnly: true
+    }
     
 },
 {
     path: '/register',
     component: AppRegister,
-    name: 'register'
+    name: 'register',
+    meta: {
+        guestOnly: true
+    }
     
 }]
 
@@ -49,3 +71,22 @@ export const router = new VueRouter({
     mode: 'history',
     routes
 })
+// export default router
+//////////////////
+// auth and guest guard 
+router.beforeEach((to, from, next) => {
+    
+    
+        //lancana provera
+        Promise.resolve(to)
+            .then(requiresAuth)
+            .then(guestOnly)
+            .then(() => {
+                console.log('router.beforeEach->Promise.resolve(to)', next)
+                next()
+            })
+            .catch(redirect => {
+                console.log('router.beforeEach->catch', redirect)
+                next(redirect);
+            })
+    })
